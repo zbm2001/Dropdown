1502ffc905fc1aa9861a5bdd00f227ed20c16f0c
ZhouBaiMin <zbm2001@aliyun.com>
2016-09-07 09:40:22 +0800

    first modify

---
+++ src/core.js
@@ -0,0 +1,29 @@
+import './util/assign';
+import typeOf from './util/typeOf';
+import create from './util/create';
+import '../lib/Component';
+import '../lib/jquery-plugins/utils';
+
+function DropDown(){  
+}
+
+Component.inherito(DropDown, {
+  
+  handleEvent(){},
+  click(){},
+  focusin(){},
+  focusout(){},
+  change(){},
+  open(){},
+  initTrigger(){},
+  renderOptions(){},
+  render(){},
+  renderOptions(){},
+  close(){},
+}, {
+
+})
+
+
+
+export default DropDown;
